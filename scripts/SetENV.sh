529303c5ba94213d0b0d2a748541aa8b901cd960
Bacteriawa <A3167717663@hotmail.com>
2025-06-26 01:55:59 +0800

    Properly upload artifact

--- scripts/SetENV.sh
+++ scripts/SetENV.sh
@@ -11,7 +11,7 @@ grdversion=$(prop version)
 preVersion=$(prop preVersion)
 release_tag="$mcversion-$commitid"
 jarName="$project_id-$mcversion"
-jarName_dir="mint-server/build/libs/$jarName.jar"
+jarName_dir="build/libs/$jarName.jar"
 make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
 
 mv build/libs/$project_id-paperclip-$grdversion-mojmap.jar $jarName_dir
