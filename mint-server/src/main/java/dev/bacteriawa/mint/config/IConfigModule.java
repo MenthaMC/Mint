a5ca746a76192bfcde23d8e252f7fe863a73f5c8
Frish2021 <1573880184@qq.com>
2025-07-09 15:33:12 +0800

    Removed IConfigModules.

--- mint-server/src/main/java/dev/bacteriawa/mint/config/IConfigModule.java
+++
@@ -1,4 +0,0 @@
-package dev.bacteriawa.mint.config;
-
-public interface IConfigModule {
-}
--- mint-server/src/main/java/dev/bacteriawa/mint/config/modules/misc/SentryConfig.java
+++ mint-server/src/main/java/dev/bacteriawa/mint/config/modules/misc/SentryConfig.java
@@ -4,11 +4,10 @@ import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import dev.bacteriawa.mint.config.ConfigCategory;
 import dev.bacteriawa.mint.config.ConfigField;
 import dev.bacteriawa.mint.config.Configuration;
-import dev.bacteriawa.mint.config.IConfigModule;
 import org.apache.logging.log4j.Level;
 
 @Configuration(name = "sentry", type = ConfigCategory.misc)
-public class SentryConfig implements IConfigModule {
+public class SentryConfig {
 
     @ConfigField(comment = {
             "Sentry DSN for improved error logging, leave blank to disable,",
