595b801e27249bff31df8df7346b650d2c2ee757
Bacteriawa <A3167717663@hotmail.com>
2025-07-20 20:01:16 +0800

    Switch to lightweight & lightclip

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java // TODO java launcher tasks
-    id("moe.luminolmc.hyacinthusweight.patcher") version "+" // Automatically pulls the latest
+    id("dev.menthamc.lightweight.patcher") version "+" // Automatically pulls the latest
 }
 
 paperweight {
