690b99301567afd60c18f942946ef138a9d490b4
Bacteriawa <A3167717663@hotmail.com>
2025-07-30 20:02:17 +0800

    Add Vanilla Fluid Pushing & Configurable collision behavior

---
+++ mint-server/src/main/java/dev/bacteriawa/mint/config/modules/fixes/CollisionBehaviorConfig.java
@@ -0,0 +1,13 @@
+package dev.bacteriawa.mint.config.modules.fixes;
+
+import dev.bacteriawa.mint.config.ConfigCategory;
+import dev.bacteriawa.mint.config.ConfigField;
+import dev.bacteriawa.mint.config.Configuration;
+
+@Configuration(name = "Collision Behavior", type = ConfigCategory.fixes)
+public class CollisionBehaviorConfig {
+    @ConfigField(comment = {"Available Value: ", "VANILLA", "BLOCK_SHAPE_VANILLA", "PAPER"})
+    public static String behaviorMode = "VANILLA";
+    @ConfigField
+    public static boolean vanillaFluidPushing = false;
+}
