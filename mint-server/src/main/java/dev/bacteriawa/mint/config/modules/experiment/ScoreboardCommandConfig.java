5ef618e1689f5e52b6c57037f9d2961c60896c87
Frish2021 <1573880184@qq.com>
2025-09-05 18:29:21 +0800

    Update ScoreboardCommandConfig.java

--- mint-server/src/main/java/dev/bacteriawa/mint/config/modules/experiment/ScoreboardCommandConfig.java
+++ mint-server/src/main/java/dev/bacteriawa/mint/config/modules/experiment/ScoreboardCommandConfig.java
@@ -7,5 +7,5 @@ import dev.bacteriawa.mint.config.Configuration;
 @Configuration(name = "enable_scoreboard_command", type = ConfigCategory.experiment)
 public class ScoreboardCommandConfig {
     @ConfigField(comment = "Enable scoreboard command support", commentZh = "启用计分板指令支持")
-    public static boolean enabled = true;
+    public static boolean enabled = false;
 }
