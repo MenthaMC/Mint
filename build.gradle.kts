b821a0287bae789afea50cc26a040b6c6cd89cea
xiaoxijun <A3167717663@hotmail.com>
2025-12-12 00:25:13 +0800

    [ci skip]Update lightweight patcher plugin version to 2.0.7-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java // TODO java launcher tasks
-    id("dev.menthamc.lightweight.patcher") version "2.0.6-SNAPSHOT"
+    id("dev.menthamc.lightweight.patcher") version "2.0.7-SNAPSHOT"
 }
 
 paperweight {
